322df8ef87b58b0b508a9e80e586e12ba44d9d23
张业鹏 <hellolittlewhite@gmail.com>
2021-11-04 09:31:40 +0800

    优化生成扑克牌的方法

--- src/constant/audio.js
+++ src/constant/audio.js
@@ -1,15 +1,3 @@
-// ui button audio
-// export const AUDIO_MAP = {
-//   default: require("@/assets/audio/btn.mp3"),
-//   call: require("@/assets/audio/angry.mp3"),
-//   notCall: require("@/assets/audio/soft.wav"),
-//   hit: require("@/assets/audio/hit.mp3"),
-//   start: require("@/assets/audio/btn.mp3"),
-// };
-
-// export function playAudio(type) {
-//   new Audio(AUDIO_MAP[type]).play();
-// }
 const softSound = loadSound(require("@/assets/audio/soft.wav"));
 const hitSound = loadSound(require("@/assets/audio/hit.mp3"));
 const angrySound = loadSound(require("@/assets/audio/angry.mp3"));
--- src/constant/poker.js
+++ src/constant/poker.js
@@ -1,36 +1,37 @@
 // 黑桃： Spade 红心：Heart 方块: Diamond 草花：Club
 const NUMS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const SUITS = ["spades", "hearts", "diams", "clubs"];
+// 所有的扑克牌
 let POKERS = [];
 
-NUMS.forEach((el) => {
-  POKERS.push({
-    rank: el,
-    suit: "spades",
-    sortKey: el,
-  });
-  POKERS.push({
-    rank: el,
-    suit: "hearts",
-    sortKey: el,
-  });
-  POKERS.push({
-    rank: el,
-    suit: "diams",
-    sortKey: el,
-  });
-  POKERS.push({
-    rank: el,
-    suit: "clubs",
-    sortKey: el,
+NUMS.forEach((num) => {
+  SUITS.forEach((suit) => {
+    POKERS.push({
+      id: num + suit,
+      rank: num,
+      suit: suit,
+      sortKey: num,
+      isSelected: false,
+    });
   });
 });
 
+// 加入大小王
 POKERS = POKERS.concat([
-  { rank: "JOKER", suit: "big", sortKey: 'big'},
-  { rank: "JOKER", suit: "little", sortKey: 'little'},
+  {
+    rank: "JOKER",
+    suit: "big",
+    sortKey: "big",
+    id: "JOKERbig",
+    isSelected: false,
+  },
+  {
+    rank: "JOKER",
+    suit: "little",
+    sortKey: "little",
+    id: "JOKERlittle",
+    isSelected: false,
+  },
 ]);
-POKERS.forEach((el) => {
-  el.id = el.rank + el.suit;
-  el.isSelected = false;
-});
+
 export default POKERS;
